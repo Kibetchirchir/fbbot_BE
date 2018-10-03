71eaa6063783965a4224d330e96f861e91d90861
Kibetchirchir <langatchirchir@gmail.com>
2018-10-03 17:58:03 +0300

    sending a request to mybot

--- app/Http/Controllers/callBack.php
+++ app/Http/Controllers/callBack.php
@@ -19,22 +19,69 @@ class callBack extends Controller
         $pid = $acc->pid;
 
         if($MpesaReceiptNumber == "FAILED"){
-            return response()->json([
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://pure-reef-27364.herokuapp.com/pay/".$pid."-0-".$Amount."",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_TIMEOUT => 30000,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    // Set Here Your Requesred Headers
+                    'Content-Type: application/json',
+                ),
+            ));
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                print_r(json_decode($response));
+            }
+            /*return response()->json([
                 'status'      => 'success',
                 'message'     => 'no failed',
                 'data'        => $pid
-            ]);
+            ]);*/
         }else{
             $balance = $acc->balance;
             $amount = $Amount + $balance;
 
             $acc->balance = $amount;
             if($acc->save()){
-                return response()->json([
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://pure-reef-27364.herokuapp.com/pay/".$pid."-1-".$Amount."",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_TIMEOUT => 30000,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_HTTPHEADER => array(
+                        // Set Here Your Requesred Headers
+                        'Content-Type: application/json',
+                    ),
+                ));
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+
+                if ($err) {
+                    echo "cURL Error #:" . $err;
+                } else {
+                    print_r(json_decode($response));
+                }
+
+                /*return response()->json([
                     'status'      => 'increased',
                     'message'     => 'saved',
                     'data'        => $pid
-                ]);
+                ]);*/
             }else
             {
                 return response()->json([
