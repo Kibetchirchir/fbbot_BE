04ac53839908ca29e44f1f2ce8c1f37d3df749d7
Kibetchirchir <langatchirchir@gmail.com>
2018-10-13 13:34:27 +0300

    Added image

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,4 +1,8 @@
 <div class="">
+    <div class="img-circle center-block">
+        <img  class="img-circle center-block" src="images/teles-bot.png" alt="Smiley face" height="50" width="50">
+        <h2 class="center-block fa-align-center"><span class="container">KUNTA</span> </h2>
+    </div>
     <ul>
         <li class="list-unstyled"><span class="">Dashboard</span> </li>
         <li class="list-unstyled"><span class="">Dashboard</span> </li>
